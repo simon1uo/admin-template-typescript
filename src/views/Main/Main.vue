25f1a12e244de2926f89dc6cc9f26cb263edd3ed
Simon Luo <simonlaw910@gmail.com>
2022-05-06 11:16:46 +0800

    build(project): initialize project config files

---
+++ src/views/Main/Main.vue
@@ -0,0 +1,17 @@
+<template>
+<div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    name: 'Main',
+    setup () {
+        return {}
+    }
+})
+</script>
+
+<style lang="less" scoped>
+
+</style>
