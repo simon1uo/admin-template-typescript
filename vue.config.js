c31274f8a9c4012b416726e3d5bc69fee40b2cf8
Simon Luo <simonlaw910@gmail.com>
2022-05-06 11:42:08 +0800

    feat: initialize project config

--- vue.config.js
+++ vue.config.js
@@ -1,50 +1,25 @@
 const path = require('path')
-const AutoImport = require('unplugin-auto-import/webpack')
-const Components = require('unplugin-vue-components/webpack')
-const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 
 module.exports = {
-  // 配置方式一：CLI提供的
-  outputDir: './build', // build输出文件夹
-  publicPath: './', // 本地build避免出错
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://152.136.185.210:5000',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': ''
+    // 配置方式一：CLI提供的
+    outputDir: './build', // build输出文件夹
+    publicPath: './', // 本地build避免出错
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://152.136.185.210:5000',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
         }
-      }
     }
-  },
-  // configureWebpack: {
-  //   resolve: {
-  //     alias: {
-  //       views: '@/views' // /src/views 的别名
-  //     }
-  //   }
-  // }
-  // configureWebpack: (config) => {
-  //   config.resolve.alias = {
-  //     '@': path.resolve(__dirname, 'src'),
-  //     views: '@/views'
-  //   }
-  // },
-  configureWebpack: {
-    plugins: [
-      AutoImport({
-        resolvers: [ElementPlusResolver()]
-      }),
-      Components({
-        resolvers: [ElementPlusResolver()]
-      })
-    ]
-  },
-  // 配置方式三 chainWebpack的函数配置/链式配置
-  chainWebpack: (config) => {
-    config.resolve.alias
-      .set('@', path.resolve(__dirname, 'src'))
-      .set('components', '@/components')
-  }
+    /*configureWebpack: {
+        chainWebpack: (config) => {
+            config.resolve.alias
+                .set('@', path.resolve(__dirname, 'src'))
+                .set('components', '@/components')
+        }
+    }*/
 }
