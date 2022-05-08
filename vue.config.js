576377db3689a6750214daa59a7ed76c19f6cf99
Simon Luo <simonlaw910@gmail.com>
2022-05-08 10:26:02 +0800

    feat: auto-import element-plus

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,18 @@
-const path = require('path')
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 
 module.exports = {
-    // 配置方式一：CLI提供的
     outputDir: './build', // build输出文件夹
     publicPath: './', // 本地build避免出错
-    devServer: {
-        proxy: {
-            '/api': {
-                target: 'http://152.136.185.210:5000',
-                changeOrigin: true,
-                pathRewrite: {
-                    '^/api': ''
-                }
-            }
-        }
+    configureWebpack: {
+        plugins: [
+            AutoImport({
+                resolvers: [ElementPlusResolver()]
+            }),
+            Components({
+                resolvers: [ElementPlusResolver()]
+            })
+        ]
     }
-    /*configureWebpack: {
-        chainWebpack: (config) => {
-            config.resolve.alias
-                .set('@', path.resolve(__dirname, 'src'))
-                .set('components', '@/components')
-        }
-    }*/
 }
