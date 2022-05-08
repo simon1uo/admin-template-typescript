be0e051a6c60e54d0dbdc708038a3e357bb99d15
Simon Luo <simonlaw910@gmail.com>
2022-05-08 10:18:40 +0800

    feat: import css

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
 import { globalRegister } from '@/global'
+import '@/assets/css/index.less'
+import 'normalize.css'
 
 createApp(App).use(globalRegister).use(store).use(router).mount('#app')
